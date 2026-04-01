a340617ff141fb3a685264566d7d3699b69e4c8a
irem <yasav07irem@hotmail.com>
2026-04-01 18:08:47 +0300

    Yapılandırma: Tarama hızı ve dizin ayarları merkezi konfigürasyon dosyasına taşındı

---
+++ ayarlar.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Nmap Tarama Hızı (T3: Normal, T4: Hızlı, T5: Agresif)
+TARAMA_HIZI = "-T4"
+
+# Varsayılan Rapor Klasörü
+RAPOR_DIZINI = "taramalar"
+
+# Versiyon ve OS Tespiti bayrakları
+DETAYLI_MOD = "-sV -O"
